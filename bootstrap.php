a0a7a1cde10c66aeedb724e4875cceb98848040f
Joshua Slui <joshua@dynamatrix.io>
2025-02-08 18:31:38 +0100

    Register the driver

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,11 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-var_dump($_ENV);
-
 $driver = (new \Aternos\Model\Driver\Mysqli\Mysqli())
     ->setUsername($_ENV['DATABASE_USER'])
     ->setPassword($_ENV['DATABASE_PASSWORD'])
     ->setDatabase($_ENV['DATABASE_NAME']);
+
+\Aternos\Model\Driver\DriverRegistry::getInstance()->registerDriver($driver);
 
 include 'src/Controllers/UrlController.php';
