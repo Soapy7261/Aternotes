0bcf8a30663b16c35e616c45c6eb4705d58e2564
Joshua Slui <joshua@dynamatrix.io>
2025-02-08 17:37:33 +0100

    Add database driver registry to bootstrapper

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,11 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+var_dump($_ENV);
+
+$driver = (new \Aternos\Model\Driver\Mysqli\Mysqli())
+    ->setUsername($_ENV['DATABASE_USER'])
+    ->setPassword($_ENV['DATABASE_PASSWORD'])
+    ->setDatabase($_ENV['DATABASE_NAME']);
+
 include 'src/Controllers/UrlController.php';
