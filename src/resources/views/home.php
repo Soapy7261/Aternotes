00c37ebb95ef4da72073972d65859c58f5e14ade
Joshua Slui <joshua@dynamatrix.io>
2025-02-10 00:22:56 +0100

    Move home.php to views

---
+++ src/resources/views/home.php
@@ -0,0 +1,22 @@
+<main>
+    <div class="body__header">
+        <div class="hero__animation">
+            <img src="src/resources/images/logo_green.png" class="body__header__image" alt="Logo">
+        </div>
+        <h2 class="hero__tagline">Tools for the people, by Aternos moderators.</h2>
+        <div class="body__header__cards">
+            <div class="body__header__card">
+                <h3>Guides</h3>
+                <p>A central location for helpful and factual community-driven guides, fact-checked by reliable members.</p>
+            </div>
+            <div class="body__header__card body__header__card--central">
+                <h3>A4M Team</h3>
+                <p>Get to know the team behind A4M and the Aternos discord's moderation and support.</p>
+            </div>
+            <div class="body__header__card">
+                <h3>Resources</h3>
+                <p>Discover Aternos and exaroton from another angle through our information and facts.</p>
+            </div>
+        </div>
+    </div>
+</main>
