41019e74ad379c7c9e0d4cf40b29c4cc55585d0f
Joshua Slui <joshua@dynamatrix.io>
2025-02-17 20:21:41 +0100

    Remove axios from scripts

--- public/index.php
+++ public/index.php
@@ -9,9 +9,8 @@ include 'bootstrap.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="src/resources/styles/root.css">
-    <script src="src/resources/javascript/logoAnimation.js"></script>
-    <script src="/node_modules/axios/dist/axios.js"></script>
+    <link rel="stylesheet" href="styles/root.css">
+    <script src="javascript/logoAnimation.js"></script>
 </head>
 <body>
     <header>
